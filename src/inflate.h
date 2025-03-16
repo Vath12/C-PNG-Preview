491911b0faf04960a033653479e9f6211740cf87
Vath12 <henry1791@icloud.com>
2025-03-15 19:46:00 -0700

    starting over with inflate

--- src/inflate.h
+++ src/inflate.h
@@ -15,22 +15,4 @@ works please read rfc 1951:
 https://www.rfc-editor.org/rfc/rfc1951
 */
 
-typedef struct BitStream{
-    uint8_t* buffer;
-    uint32_t byte; 
-    uint8_t bit;
-} BitStream;
-
-typedef struct DeflateHeader{
-    uint8_t compressionMethod;
-    uint8_t compressionInfo;
-    uint8_t hasDictionary;
-    uint8_t compressionLevel;
-    uint8_t isValid;
-} DeflateHeader;
-
-DeflateHeader readHeader(uint8_t *buffer);
-uint8_t nextBit(BitStream *stream);
-uint32_t nextBits(BitStream *stream,int num);
-
 int inflate(uint8_t *buffer,size_t size,uint8_t **output);
