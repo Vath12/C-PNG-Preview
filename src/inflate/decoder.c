f7760e88ff4e7c6e1a5e62dc463753d043894ff8
Vath12 <henry1791@icloud.com>
2025-03-17 13:16:23 -0700

    Added bit utility methods to decoder.c

--- src/inflate/decoder.c
+++ src/inflate/decoder.c
@@ -7,6 +7,53 @@ struct prefixAlphabet{
     uint16_t *literals;
     uint16_t *codes;
 };
+/*
+needed because bytes being in network order
+cause both memcpy and casting a pointer to reverse the result
+*/
+static void revmemcpy(void *dest, void *src, size_t length){
+    char *d = dest;
+    char *s = src;
+    for (int i = 0; i < length; i++){
+        d[i] = s[length-1-i];
+    }
+}
+
+/*
+bits are read from the byte in this order:
+MSB | 7 6 5 4 3 2 1 0 | LSB
+
+Packing:
+
+Packed into the output number beginning with the LSB
+ex.
+read 4 bits from 00001000
+output: 1000
+
+Packed into the output number beginning with the MSB
+ex.
+read 4 bits from 00001000
+output: 0001
+*/
+uint8_t getBit(uint8_t *buffer,uint64_t ptr){
+    return (buffer[ptr/8] >> (ptr%8)) & 0b1;
+}
+uint32_t getBitsLSB(uint8_t *buffer,uint64_t ptr,uint8_t num){
+    uint32_t out = 0;
+    for (uint8_t i = 0; i < num; i++){
+        out <<= 1;
+        out |= getBit(buffer,ptr+num-i-1);
+    }   
+    return out;
+}
+uint32_t getBitsMSB(uint8_t *buffer,uint64_t ptr,uint8_t num){
+    uint32_t out = 0;
+    for (uint8_t i = 0; i < num; i++){
+        out <<= 1;
+        out |= getBit(buffer,ptr+i);
+    }   
+    return out;
+}
 
 //see rfc 1951 pg 7
 int generateCodes(struct prefixAlphabet *alphabet, uint8_t *lengths, uint16_t size){
@@ -41,6 +88,7 @@ int generateCodes(struct prefixAlphabet *alphabet, uint8_t *lengths, uint16_t si
     return 1;
 }
 
-int deflate(uint8_t *out,uint64_t *outputLength,uint8_t *src,uint64_t srcLength){
+int deflate(uint8_t *out,size_t *outputLength,uint8_t *src,size_t srcLength){
+
     return 1;
 }
