b302298a1f0a9452cc1944722f545ae5c978ad3f
Vath12 <henry1791@icloud.com>
2025-03-16 19:56:12 -0700

    Replaced bitwise ops with reverse memcpy in readChunk

--- src/pngChunks.c
+++ src/pngChunks.c
@@ -5,14 +5,23 @@ const char* CHUNK_CODE_IHDR = "IHDR";
 const char* CHUNK_CODE_IDAT = "IDAT";
 const char* CHUNK_CODE_PLTE = "PLTE";
 
+/*
+needed because all the bytes are in network order
+so both memcpy and casting a pointer reverse the result
+*/
+static void revmemcpy(void *dest, void *src, size_t length){
+    char *d = dest;
+    char *s = src;
+    for (int i = 0; i < length; i++){
+        d[i] = s[length-1-i];
+    }
+}
+
 //TODO: improve aesthetic of ptr+= (very yucky)
 ChunkData readChunk(uint8_t *stream,uint32_t ptr){
     ChunkData data = {0,{0,0,0,0},NULL,0};
-    data.length = // length (4 byte unsigned int)
-        (stream[ptr] << 24) | 
-        (stream[ptr + 1] << 16) | 
-        (stream[ptr + 2] << 8) | 
-        (stream[ptr + 3]); 
+
+    revmemcpy(&data.length,&stream[ptr],4);
     ptr += 4;
     memcpy(data.code,&stream[ptr],4); // the chunk's 4 character ASCII ID
     data.code[4] = '\0';
@@ -22,24 +31,9 @@ ChunkData readChunk(uint8_t *stream,uint32_t ptr){
     //TODO: verify checksum is correct given the data 
     //see section 3.4
     //http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html#CRC-algorithm
-    data.CRC = //cyclic rendundancy code (4 byte unsigned int)
-        (stream[ptr] << 24) | 
-        (stream[ptr + 1] << 16) | 
-        (stream[ptr + 2] << 8) | 
-        (stream[ptr + 3]); 
+    //cyclic rendundancy code (4 byte unsigned int)
+    revmemcpy(&data.CRC,&stream[ptr],4);
     return data;
-}
-
-/*
-needed because all the bytes are in network order
-so both memcpy and casting a pointer reverse the result
-*/
-void revmemcpy(void *dest, void *src, size_t length){
-    char *d = dest;
-    char *s = src;
-    for (int i = 0; i < length; i++){
-        d[i] = s[length-1-i];
-    }
 }
 
 int parseIHDR(ChunkData* c, IHDR *output){
